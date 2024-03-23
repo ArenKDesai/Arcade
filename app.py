7783d19e3340439ef91cfc7bde406f4671255bc1
Aren Desai <akdesai@wisc.edu>
2024-03-22 20:38:44 -0500

    attempting artwork

--- app.py
+++ app.py
@@ -14,10 +14,13 @@ color2 = pygame.Color(255, 255, 255)   # White
 color3 = pygame.Color(128, 128, 128)   # Grey
 color4 = pygame.Color(62, 180, 137)    # Mint
 
+test = False
+
 FPS = pygame.time.Clock()
 
 def start_game():
     # Character Selection
+    test = True
     character1 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((100, 50), (150, 200)),
                                             text='C1',
                                             manager=manager)
@@ -36,7 +39,16 @@ def start_game():
     character6 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((700, 300), (150, 200)),
                                             text='C6',
                                             manager=manager)
-    return
+    return character1, character2, character3, character4, character5, character6
+
+def select_attribute(character1, character2, character3, character4, character5, character6):
+    character1.kill()
+    character2.kill()
+    character3.kill()
+    character4.kill()
+    character5.kill()
+    character6.kill()
+    # Attribute Selection
 
 running = True
 if __name__ == "__main__":
@@ -52,10 +64,33 @@ if __name__ == "__main__":
             elif event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == start_button:
                     start_button.kill()
-                    start_game()
+                    # TODO: make this suck less
+                    character1, character2, character3, character4, character5, character6 = start_game()
                 elif event.ui_element == character1:
                     print("Character 1 selected")
+                    select_attribute(character1, character2, character3, character4, character5, character6)
+                elif event.ui_element == character2:
+                    print("Character 2 selected")
+                    select_attribute(character1, character2, character3, character4, character5, character6)
+                elif event.ui_element == character3:
+                    print("Character 3 selected")
+                    select_attribute(character1, character2, character3, character4, character5, character6)
+                elif event.ui_element == character4:
+                    print("Character 4 selected")
+                    select_attribute(character1, character2, character3, character4, character5, character6)
+                elif event.ui_element == character5:
+                    print("Character 5 selected")
+                    select_attribute(character1, character2, character3, character4, character5, character6)
+                elif event.ui_element == character6:
+                    print("Character 6 selected")
+                    select_attribute(character1, character2, character3, character4, character5, character6)
             manager.process_events(event)
+        if test:
+            button_image = pygame.image.load("artwork/cat_icon.jpg").convert_alpha()
+            button_rect = button_image.get_rect()
+            button_rect.topleft = character1.rect.topleft
+            DISPLAYSURF.blit(button_image, button_rect)
+
         # Reset the screen
         manager.update(time_delta)
         DISPLAYSURF.fill(color1)
