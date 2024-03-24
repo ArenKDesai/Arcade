8f0b8ee34acd5d116856b404c28ee957bb7b4bd3
Aren Desai <akdesai@wisc.edu>
2024-03-24 18:31:31 -0500

    test

--- app.py
+++ app.py
@@ -148,7 +148,9 @@ if __name__ == "__main__":
                 joystick = pygame.joystick.Joystick(event.device_index)
             if joystick:
                 # sending controller input to controller_input
-                print(joystick.get_numbuttons())
+                for button in joystick.get_numbuttons():
+                    if joystick.get_button(button):
+                        print(f'Button {button} pressed')
                 selected_button = gameplay.controller_input(event.type, selected_button)
             elif event.type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == start_button:
