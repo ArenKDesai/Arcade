d7ef20fd9e966e4e25972d61d199a298bd18de38
Aren Desai <akdesai@wisc.edu>
2024-03-24 19:35:54 -0500

    Update selected_button print statement

--- gameplay.py
+++ gameplay.py
@@ -141,7 +141,7 @@ class BoundryButton:
 # Output: hovers and unhovers buttons
 def controller_input(x_axis, y_axis, selected_button):
     print(x_axis, y_axis)
-    print(f'selected_button: {selected_button.get_button().get_text()}')
+    print(f'selected_button: {selected_button.get_button()}')
     if (x_axis > -0.9 and selected_button.get_left() != None):
         selected_button.get_button().unselect()
         selected_button = selected_button.get_left()
